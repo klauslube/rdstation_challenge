71af9681ab7db3ffc548d70858c48383f0489349
klauslube <klauslube93@gmail.com>
2025-10-29 21:34:44 -0300

    feat: create CartItem model and associations

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,6 @@
 class Cart < ApplicationRecord
+  has_many :cart_items, dependent: :destroy
+  has_many :products, through: :cart_items
+
   validates :total_price, numericality: { greater_than_or_equal_to: 0 }
 end
