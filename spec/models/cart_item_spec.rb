6502bc31e289bfad9e2ef3597213d84f59fb240d
klauslube <klauslube93@gmail.com>
2025-10-30 01:00:49 -0300

    test: add validations tests for cart item

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -1,8 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe CartItem, type: :model do
-  describe 'associations' do
-    it { should belong_to(:product) }
-    it { should belong_to(:cart) }
+  context 'when validating' do
+    it 'is valid with right attributes' do
+      cart_item = build(:cart_item)
+      expect(cart_item).to be_valid
+    end
+
+    it 'validates quantity is greater than 0' do
+      cart_item = build(:cart_item, quantity: 0)
+      expect(cart_item).to_not be_valid
+      expect(cart_item.errors[:quantity]).to include("must be greater than 0")
+    end
+
+    it 'validates quantity is an integer' do
+      cart_item = build(:cart_item, quantity: 7.5)
+      expect(cart_item).to_not be_valid
+      expect(cart_item.errors[:quantity]).to include("must be an integer")
+    end
+
+    it 'validates uniqueness of product_id scoped to cart_id' do
+      cart = create(:cart)
+      product = create(:product)
+      
+      create(:cart_item, cart: cart, product: product)
+      
+      duplicate_item = build(:cart_item, cart: cart, product: product)
+      expect(duplicate_item).to_not be_valid
+      expect(duplicate_item.errors[:product_id]).to include("has already been taken")
+    end
+
+    describe 'associations' do
+      it { should belong_to(:product) }
+      it { should belong_to(:cart) }
+    end 
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,7 @@ require_relative '../config/environment'
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+require 'factory_bot_rails'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -39,7 +39,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
+  config.include FactoryBot::Syntax::Methods
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
